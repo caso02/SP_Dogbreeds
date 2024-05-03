739b373449193e718090a0f4314c4cf1dbf166ef
caso <lucashersche@gmail.com>
2024-05-03 11:24:03 +0200

    Save data in database, show data from database on website

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+import sqlite3
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -7,6 +9,13 @@ app = Flask(__name__)
 def hello():
     return render_template('index.html')
 
-@app.route('/about/')
-def about():
-    return render_template('about.html')
+@app.route('/data/')
+def data():
+    # Verbindung zur SQLite-Datenbank mit dem vollständigen Pfad
+    conn = sqlite3.connect('data/dog_breeds.db')
+    # Daten abfragen
+    df = pd.read_sql_query('SELECT * FROM cleaned_data', conn)
+    # Verbindung schließen
+    conn.close()
+    # Daten an das Template senden (HTML-Tabelle erstellen oder ähnliches)
+    return render_template('data.html', tables=[df.to_html(classes='data', escape=False)], titles=df.columns.values)
